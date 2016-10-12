757f63a8a87b1656ef5061674e73e5c075a3c8ff
Chris Duranti <chris.duranti@socrata.com>
2016-10-11 17:10:19 -0700

    wip

--- lib/joinery/join.ex
+++ lib/joinery/join.ex
@@ -1,11 +1,68 @@
 defmodule Joinery.Join do
+  alias Joinery.Pager
 
+
+  defp advance([row | _] = rows, join_on) do
+    value = Map.get(row, join_on)
+    Enum.split_while(rows, fn %{^join_on => rest_value} ->
+      value == rest_value
+    end)
+  end
+
+  # When both subsets are empty, advance both left and right
+  defp join_maps([], [], left, right, join_on) do
+    {left_subset, left_rest} = advance(left, join_on)
+    {right_subset, right_rest} = advance(right, join_on)
+
+    join_maps(left_subset, right_subset, left_rest, right_rest, join_on)
+  end
+
+  defp join_maps([], right_subset, [left_head | left_rest], right, join_on) do
+
+    join_maps([left_head], right, left_rest, right, join_on)
+  end
+
+  defp join_maps(left_subset, [], left, [right_head | right_rest], join_on) do
+    join_maps(left_subset, [right_head], left, right_rest, join_on)
+  end
+
+  defp join_maps(left_subset, right_subset, left, right, join_on) do
+    left_value = left[join_on]
+    right_value = right[join_on]
+
+    if left_value == right_value do
+
+    end
+
+    IO.inspect left
+    IO.inspect right
+  end
+
+
+
+  defp join_maps(left, right, join_on) do
+    join_maps([], [], left, right, join_on)
+  end
+
+
+
+  defp to_maplist(rows) do
+    Enum.map(rows, fn row -> Enum.into(row, %{}) end)
+  end
 
   def join(left_pager, right_pager, join_on) do
-    # Stream.transform(left, right, fn left_row, right ->
-    #   Stream.transform(right, [], fn right_row, acc ->
-    #     IO.inspect "Row"
-    #   end)
-    # end)
+    with {:ok, left} <- Pager.next(left_pager),
+      {:ok, right} <- Pager.next(right_pager) do
+
+      join_maps(
+        to_maplist(left),
+        to_maplist(right),
+        join_on
+      )
+    end
   end
+
+
+
+
 end
--- test/join_test.exs
+++ test/join_test.exs
@@ -1,36 +1,62 @@
+defmodule FastPager do
+  def start(sort_key, rows) do
+    sorted_rows = Enum.sort_by(rows, fn keylist ->
+      {_, value} = Enum.find(keylist, fn {a_key, _} -> a_key == sort_key end)
+      value
+    end)
+
+    spawn_link(
+      FastPager,
+      :loop,
+      [rows]
+    )
+  end
+
+  def loop([]) do
+    :done
+  end
+
+  def loop([row | rest]) do
+    receive do
+      {:fetch_next, sender_pid} ->
+        response = {:ok, [row]}
+        send sender_pid, {:fetched, response}
+        loop(rest)
+    end
+  end
+end
+
+
 defmodule JoinTest do
   use ExUnit.Case
   alias Joinery.Join
 
   def sort_by_key(rows, key) do
-    Enum.sort_by(rows, fn keylist ->
-      {_, value} = Enum.find(keylist, fn {a_key, _} -> a_key == key end)
-      value
-    end)
+
   end
 
+
   test "can do a simple join" do
-    left = [
-      [{"name", "chris"}, {"emoji", ":dumpsterfire:"}],
-      [{"name", "urmi"}, {"emoji", ":confused-yay:"}],
-      [{"name", "pete"}, {"emoji", ":elm:"}],
-      [{"name", "cate"}, {"emoji", ":cat:"}],
+    left_pager = FastPager.start("emoji", [
+      # [{"name", "chris"}, {"emoji", ":dumpsterfire:"}],
+      # [{"name", "urmi"}, {"emoji", ":confused-yay:"}],
+      # [{"name", "pete"}, {"emoji", ":elm:"}],
+      # [{"name", "cate"}, {"emoji", ":cat:"}],
       [{"name", "kaida"}, {"emoji", ":tofu:"}],
       [{"name", "michael"}, {"emoji", ":rainier:"}],
-      [{"name", "robert"}, {"emoji", ":haskell:"}]
-    ] |> sort_by_key("emoji")
+      [{"name", "robert"}, {"emoji", ":tofu:"}]
+    ])
 
-
-    right = [
-      [{"image", "dumpsterfire.png"}, {"emoji", ":dumpsterfire:"}],
-      [{"image", "confused_yay.png"}, {"emoji", ":confused-yay:"}],
-      [{"image", "elm.png"}, {"emoji", ":elm:"}],
-      [{"image", "cat.png"}, {"emoji", ":cat:"}],
+    right_pager = FastPager.start("emoji", [
+      # [{"image", "dumpsterfire.png"}, {"emoji", ":dumpsterfire:"}],
+      # [{"image", "confused_yay.png"}, {"emoji", ":confused-yay:"}],
+      # [{"image", "elm.png"}, {"emoji", ":elm:"}],
+      # [{"image", "cat.png"}, {"emoji", ":cat:"}],
       [{"image", "tofu.png"}, {"emoji", ":tofu:"}],
-      [{"image", "rainier.png"}, {"emoji", ":rainier:"}],
-      [{"image", "haskell.png"}, {"emoji", ":haskell:"}]
-    ] |> sort_by_key("emoji")
+      [{"image", "rainier.png"}, {"emoji", ":rainier:"}]
+    ])
 
-    Join.join(left, right, "image")
+
+    Join.join(left_pager, right_pager, "emoji")
   end
 end
